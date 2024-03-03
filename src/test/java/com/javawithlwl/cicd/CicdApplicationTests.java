fc9bcff84b26bf901884c2f65cc355c720fbefdc
javawithlwl <javawithlwl@gmail.com>
2024-03-03 09:45:00 +0530

    Learning CI and CD

---
+++ src/test/java/com/javawithlwl/cicd/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javawithlwl.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
